669dcdf916e721c73c05b7efc6847c5cf6fdd8d5
Mattia <87669872+mattiabresciani@users.noreply.github.com>
2025-02-12 09:33:28 +0100

    Create menu.py
    Creazione del menu che verrà visualizzato nel main

---
+++ menu.py
@@ -0,0 +1,10 @@
+def stampamenu():
+  print("MENU AZIONI\n")
+  print("[1] Modifica della quantità di un articolo.")
+  print("[2] Aggiungi o rimuovi un articolo.")
+  print("[3] Visualizza articoli di una categoria.")
+  print("[4] Aggiungi o rimuovi una categoria.")
+  print("[5] Salva su un file txt.\n")
+  print("[6] Visualizza crediti.")
+  print("[0] Esci dal programma.")
+  
