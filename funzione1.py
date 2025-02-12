addf99247b6c4b44e4394b8c370064af3016618d
saladingo1 <20899@studenti.marconiverona.edu.it>
2025-02-12 09:35:18 +0100

    funzione1.py
    apre il file richiesto in lettura, salva i dati in una lista, modifica la lista tramite gli input dati, riscrive il file secondo la nuova lista "righe"

--- funzione1.py
+++ funzione1.py
@@ -1,1 +1,14 @@
-
+def funzione1():
+  categoria=input("Di quale categoria fa parte il tuo articolo: ")
+  f=open(categoria+"csv","r")
+  articolo=input("Quale articolo vuoi modificare? ")
+  quantità=int("Qual è la nuova quantità? ")
+  righe=f.readlines()
+  f.close()
+  for count in range(0,len(righe)):
+    if righe[count[0]]==articolo:
+      righe[count[1]]=quantità
+  f=open(categoria+"csv","w")
+  f.writelines(righe)
+  f.close()
+  
