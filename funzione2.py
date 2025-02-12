169076a7a5a5b33ac02f5802ec217468f145430e
bendo08 <20655@studenti.marconiverona.edu.it>
2025-02-12 09:38:25 +0100

    Update funzione2.py
    nessuna modifica effettuata

--- funzione2.py
+++ funzione2.py
@@ -1,1 +1,13 @@
-
+def function2():
+  categoria=input("Inserisci la categoria dell'articolo che vuoi comprare casalinghi-cartoleria-abbigliamento")
+  if categoria == "cartoleria":
+    artcolo = input("Inserisci un articolo da aggiungiere o da rimuovere")
+    f = open("cartoleria.csv","w")
+    riga = f.readline()
+    if riga != "":
+      print(riga)
+      riga = f.readline()
+      f.close()
+                  
+                  
+                        
