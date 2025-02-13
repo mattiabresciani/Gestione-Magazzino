2835afc4fe9e6cefcd06f20ed5a8cf7507932d15
Mattia <87669872+mattiabresciani@users.noreply.github.com>
2025-02-13 18:33:44 +0100

    Update funzione4.py
    inserito autore + cambiato il nome della funzione

--- funzione4.py
+++ funzione4.py
@@ -1,8 +1,10 @@
 import os
-def funzione4():
+def adddellcateg():
     categoria = input("Inserisci il nome della categoria che vuoi aggiungere o rimuovere: ")
     if os.path.exists(categoria+".csv"):
         os.remove(categoria+".csv")
     else:
         f=open(categoria+".csv","w")
         f.close()
+
+#Matteo Saladino
