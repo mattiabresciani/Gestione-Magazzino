1d8a3a6ba17bec83260ef8cb87355d0996241c73
saladingo1 <20899@studenti.marconiverona.edu.it>
2025-02-13 17:27:05 +0100

    funzione1.py

--- funzione1.py
+++ funzione1.py
@@ -10,10 +10,10 @@ def funzione1():
     lista.append(dati)
     riga=f.readline()
   f.close()
-  for count in range(0,len(lista)):
-    if lista[count][0]==articolo:
-      lista[count][1]=quantità
+  for i in range(0,len(lista)):
+    if lista[i][0]==articolo:
+      lista[i][1]=quantità
   f=open(categoria+".csv","w")
-  f.writelines(str(lista))
+  for elemento in lista:
+    f.write(",".join(elemento)+ "\n")
   f.close()
-
