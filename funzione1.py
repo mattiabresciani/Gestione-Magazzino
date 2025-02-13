09674a2fa52d9e9800e1def07efa51660d770ab0
saladingo1 <20899@studenti.marconiverona.edu.it>
2025-02-13 16:41:54 +0100

    funzione1.py

--- funzione1.py
+++ funzione1.py
@@ -1,14 +1,19 @@
 def funzione1():
+  lista=[]
   categoria=input("Di quale categoria fa parte il tuo articolo: ")
-  f=open(categoria+"csv","r")
+  f=open(categoria+".csv","r")
   articolo=input("Quale articolo vuoi modificare? ")
-  quantità=int("Qual è la nuova quantità? ")
-  righe=f.readlines()
+  quantità=int(input("Qual è la nuova quantità? "))
+  riga=f.readline()
+  while riga!="":
+    dati=riga.strip().split(",")
+    lista.append(dati)
+    riga=f.readline()
   f.close()
-  for count in range(0,len(righe)):
-    if righe[count[0]]==articolo:
-      righe[count[1]]=quantità
-  f=open(categoria+"csv","w")
-  f.writelines(righe)
+  for count in range(0,len(lista)):
+    if lista[count][0]==articolo:
+      lista[count][1]=quantità
+  f=open(categoria+".csv","w")
+  f.writelines(str(lista))
   f.close()
-  
+
