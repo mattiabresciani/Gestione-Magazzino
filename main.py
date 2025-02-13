cee1a9de46fc1b5f92ad10c363aa5e0dc84dadb5
Mattia <87669872+mattiabresciani@users.noreply.github.com>
2025-02-13 18:50:12 +0100

    Update main.py
    Risoluzione errore loop, adesso continua a ciclare all'infinito.

--- main.py
+++ main.py
@@ -5,42 +5,32 @@ import funzione4
 import funzione5
 import menu
 
-#file main
-reload = True #reload sarà la variabile che permetterà di usare il programma all'infinito. 
+esegui = True  #variabile di controllo per mantenere il loop attivo
 
-while reload == True:
-  try: #prova a chiedergli in input la scelta del menu
-    print(menu.stampamenu()) #visualizza il menu
-    scelta = int(input("Inserisci l'opzione \n >>    "))
-    reload = False
-  except ValueError: #se l'utente inserisce un numero float
-    print("Errore nell'inserimento del comando. Ricontrolla.")
-    reload = True
-  except TypeError: #se l'utente inserisce una stringa
-    print("Errore nell'inserimento del comando. Ricontrolla.")
-    reload = True
+while esegui:
+    try:
+        print(menu.stampamenu())  #visualizza il menu
+        scelta = int(input("Inserisci l'opzione \n >>    "))
+    except ValueError:  #se l'utente inserisce un valore non intero
+        print("Errore nell'inserimento del comando. Ricontrolla.")
+        continue  #torna all'inizio del loop
+    except TypeError:
+        print("Errore nell'inserimento del comando. Ricontrolla.")
+        continue
 
-
-#Inizio parte della selezione e dei controlli. (gestione input)
-if scelta == 0:
-  print("Fine esecuzione programmi")
-  reload = False #siccome non ci sarà nulla da fare il programma si chiude
-elif scelta == 1:
-  funzione1.addarticolo()
-  reload = True
-elif scelta == 2:
-  funzione2.acquisto()
-  reload = True
-elif scelta == 3:
-  funzione3.visualizzacategoria()
-  reload = True
-elif scelta == 4:
-  funzione4.adddellcateg()
-  reload = True
-elif scelta == 5:
-  funzione5.savetxt()
-  reload = True
-
-else:
-  print("Comando non riconosciuto")
-  reload = True
+    #gestione delle scelte
+    if scelta == 0:
+        print("Fine esecuzione programma")
+        esegui = False  #imposta la variabile per uscire dal loop
+    elif scelta == 1:
+        funzione1.addarticolo()
+    elif scelta == 2:
+        funzione2.acquisto()
+    elif scelta == 3:
+        funzione3.visualizzacategoria()
+    elif scelta == 4:
+        funzione4.adddellcateg()
+    elif scelta == 5:
+        funzione5.savetxt()
+    else:
+        print("Comando non riconosciuto")
